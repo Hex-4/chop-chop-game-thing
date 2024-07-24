4d4f3f29b6e700ae8c7cfb6b899cfd3333159dcf
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2024-07-24 11:03:59 -0600

    fancy dragging

--- card.gd
+++ card.gd
@@ -1,30 +1,34 @@
 extends StaticBody2D
 
 
-var can_drag = false
-var mouse_offset
-var lmps = []
 
-func _ready():
-	for i in 5:
-		lmps.append(get_global_mouse_position())
+
+# modified code from gemmomoh on godot forums
+var canDrag = false
+var cards_that_are_not_being_dragged = []
+
+
+func _process(delta):
+	print(self)
+	if canDrag:
+		$".".global_position = lerp($".".global_position, get_global_mouse_position(), 8 * delta)
 
 
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		var tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
-		tween.tween_property(self, "position", get_global_mouse_position(), 0.2)
-		tween.tween_callback(self.enable_drag)
-
-func enable_drag():
-	can_drag = true
-
-func _process(delta):
-	lmps.remove_at(0)
-	lmps.append(get_global_mouse_position())
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or can_drag:
-
-		print(mouse_offset)
-		#mouse_offset *= -1
-		print(mouse_offset)
-		position = lmps[0]
+	if event is InputEventMouseMotion or InputEventMouseButton:
+		if event.button_mask == 1:
+			cards_that_are_not_being_dragged = []
+			for i in get_tree().get_nodes_in_group("card"):
+				if i.canDrag == false or i == self:
+					cards_that_are_not_being_dragged.append(i)
+			if len(cards_that_are_not_being_dragged) == len(get_tree().get_nodes_in_group("card")):
+				canDrag = true
+				z_index = 100
+				
+				
+				
+			
+			
+		else:
+			z_index = 0
+			canDrag = false
